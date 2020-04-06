7ea44e6810706f771f012b99ba9977a090bd3298
Black Formatter for Python <black-format@1337z.ninja>
2020-04-06 12:20:13 +0000

    Format Python code

--- django/coco_helvetica_gazette/settings.py
+++ django/coco_helvetica_gazette/settings.py
@@ -158,9 +158,7 @@ else:
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static_out")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Enable redirect to https on Google App Engine
 if os.getenv("GAE_INSTANCE"):
--- django/coco_helvetica_gazette/urls.py
+++ django/coco_helvetica_gazette/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import re_path, path, include
 
 from django.views.generic.base import RedirectView
-favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+
+favicon_view = RedirectView.as_view(url="/static/favicon.ico", permanent=True)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pages.urls")),
-    re_path(r'^favicon\.ico$', favicon_view),
+    re_path(r"^favicon\.ico$", favicon_view),
 ]
