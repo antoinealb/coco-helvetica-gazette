549635c6c565836ad5a3fc318d886755e8e4e824
Antoine Albertelli <antoinealb@google.com>
2020-04-07 16:22:10 +0200

    Deploy favicon ico correctly

--- django/coco_helvetica_gazette/urls.py
+++ django/coco_helvetica_gazette/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import re_path, path, include
+from django.templatetags.static import static
 
 from django.views.generic.base import RedirectView
 
-favicon_view = RedirectView.as_view(url="/static/favicon.ico", permanent=True)
+favicon_view = RedirectView.as_view(url=static("favicon.ico"), permanent=True)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
